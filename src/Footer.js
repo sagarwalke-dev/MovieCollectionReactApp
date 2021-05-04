a3d704b3c473b03dfc23853254eb90fb90ab6070
Sagar <sagarwalke.web@gmail.com>
2021-05-04 13:03:57 +0530

    Initial commit

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        Develop by{" "}
+        <a href='https://sagarwalke-dev.github.io/' target='_blank'>
+          sagarwalke.dev
+        </a>{" "}
+        | made with ❤️
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <div className='header'>sushant singh rajput movies collection</div>
+    </>
+  );
+}
+
+export default Header;
